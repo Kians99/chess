19da9b619b51218dedae7bab8defb184e92472b2
Kian <Ksadeghi@sas.upenn.edu>
2020-10-29 12:01:58 -0400

    Make letter to number a class variable

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -2,17 +2,26 @@ class Piece
 
   attr_reader :color, :unicode
 
+  @@letter_to_number = { "A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6, "G" => 7, "H" => 8}
+
   def initialize(color, unicode)
     @color = color
     @unicode = unicode
   end
 
+  def self.letter_to_number
+    @@letter_to_number
+  end
+
   def translate_to_numerical(coord)
     num_val = []
-    letter_to_number = { "A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6, "G" => 7, "H" => 8}
-    num_val[0] = letter_to_number[coord[0]]
+    num_val[0] = Piece.letter_to_number[coord[0]]
     num_val[1] = coord[1].to_i
     num_val
+  end
+
+  def translate_to_algebraic(coord)
+    coord[1].to_s + Piece.letter_to_number.key(coord[0]) 
   end
 
 
