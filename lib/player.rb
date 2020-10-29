05c489ab7c81a3d17d3368e8854c9e90d5d9baec
Kian <Ksadeghi@sas.upenn.edu>
2020-10-29 15:37:24 -0400

    Add direction of piece movement

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,11 @@
 class Player
 
-  attr_reader :color, :number
+  attr_reader :color, :number, :direction
 
-  def initialize(color, number)
+  def initialize(color, number, direction)
     @color = color
     @number = number
+    @direction = direction
   end
 
   def make_guess
