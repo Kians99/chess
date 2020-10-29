b3bde3dddbb7cfba5dab932da8c038730827790d
Kian <Ksadeghi@sas.upenn.edu>
2020-10-29 11:33:46 -0400

    Translate algebraic to numerical

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -6,5 +6,15 @@ class Piece
     @color = color
     @unicode = unicode
   end
+
+  def translate_to_numerical(coord)
+    num_val = []
+    letter_to_number = { "A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6, "G" => 7, "H" => 8}
+    num_val[0] = letter_to_number[coord[0]]
+    num_val[1] = coord[1].to_i
+    num_val
+  end
+
+
 end
 
