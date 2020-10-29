fbc1a6cd5eb0ee2b22c0458a921a4c28c628482d
Kian <Ksadeghi@sas.upenn.edu>
2020-10-29 15:38:00 -0400

    Numerical to algebraic test

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -3,28 +3,24 @@ require './lib/pieces/piece'
 describe Piece do
   describe "#translate_to_numerical" do
     it "returns algebraic notation to numerical" do
-      piece = Piece.new("B", "\u2655")
-      translation = piece.translate_to_numerical('A4')
+      translation = Piece.translate_to_numerical('A4')
       p translation
       expect(translation).to eql([1, 4])
     end
 
     it "returns algebraic notation to numerical" do
-      piece = Piece.new("B", "\u2655")
-      translation = piece.translate_to_numerical('H8')
+      translation = Piece.translate_to_numerical('H8')
       p translation
       expect(translation).to eql([8, 8])
     end
 
     it "returns numerical to algebraic notation" do
-      piece = Piece.new("B", "\u2655")
-      translation = piece.translate_to_algebraic([8,8])
+      translation = Piece.translate_to_algebraic([8,8])
       expect(translation).to eql('H8')
     end
 
     it "returns numerical to algebraic notation" do 
-      piece = Piece.new("B", "\u2655")
-      translation = piece.translate_to_algebraic([2,5])
+      translation = Piece.translate_to_algebraic([2,5])
       expect(translation).to eql('B5')
     end
   end
