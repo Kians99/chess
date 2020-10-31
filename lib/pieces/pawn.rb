6989e508087f5773178aa6cdd4f92982e01b6e3f
Kian <Ksadeghi@sas.upenn.edu>
2020-10-31 19:29:39 -0400

    Add call to super function

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -9,30 +9,31 @@ class Pawn < Piece
     @name = 'pawn'
   end
 
+  #ADD ABILIRY TO MOVE DIAGNOLALY 
+
   def valid_move(start_coord, end_coord, player)
-    num_start = Piece.translate_to_numerical(start_coord)
-    moves = possible_moves(num_start, player)
-    num_end = Piece.translate_to_numerical(end_coord)
-    moves.include?(num_end)
+    #to_numerical[0] = start coordinate
+    #to_numerical[1] = end coordinate
+    to_numerical = super(start_coord, end_coord)
+    moves = possible_moves(to_numerical[0], player)
+    moves.include?(to_numerical[1])
   end
 
-  private
-
   def possible_moves(coord, player, pos_moves = [])
-    p player.direction
     first_pos_move = coordinates(player.direction * 1, coord)
     pos_moves.push(first_pos_move)
     if coord[1] == 2 || coord[1] == 7
       second_pos_move = coordinates(player.direction * 2, coord)
       pos_moves.push(second_pos_move)
     end
-    p pos_moves
     pos_moves
   end
 
-  def coordinates(spaces, orig_coord, new_coord = [])
+  private
+
+  def coordinates(direction, orig_coord, new_coord = [])
     new_coord[0] = orig_coord[0]
-    new_coord[1] = orig_coord[1] + spaces
+    new_coord[1] = orig_coord[1] + direction
     new_coord
   end
 
