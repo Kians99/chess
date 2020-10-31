7c79272ae4ab9b0d57872e02ec22ceda8939c872
Kian <Ksadeghi@sas.upenn.edu>
2020-10-31 16:40:41 -0400

    Check if coordinate is valid

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -24,6 +24,20 @@ class Piece
     Piece.letter_to_number.key(coord[0]) + coord[1].to_s
   end
 
+  def valid_move(start_coord, end_coord)
+    num_start = Piece.translate_to_numerical(start_coord)
+    num_end = Piece.translate_to_numerical(end_coord)
+    [num_start, num_end]
+  end
+
+  def valid_coord?(x_val, y_val)
+    x_val.between?(1, 8) && y_val.between?(1, 8)
+  end
+
+  #Add funtion that cleans the possible inputs
+  #add function that checks to see what kind of piece
+  #is already at the avaible inputs
+
 
 end
 
