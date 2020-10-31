2354248995c8044700b7b27b815854befdf0611e
Kian <Ksadeghi@sas.upenn.edu>
2020-10-31 19:30:05 -0400

    Minor changes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require './lib/game'
 
 describe Game do
-  describe "ensures proper coordinate input" do
+  describe '#proper_move_format?' do
     it "disallows lowercase coordinates" do
       game = Game.new
       expect(game.proper_move_format?("a6 b7")).to eql(false)
@@ -43,9 +43,60 @@ describe Game do
     end
   end
 
-  
+  describe '#player_piece' do
+    it "Allows player white to move white piece" do
+      game = Game.new
+      player = Player.new("W", 1, 1)
+      result = game.player_piece("D1", player)
+      expect(result).not_to eql(nil)
+    end
+
+    it "Allows player black to move black piece" do
+      game = Game.new
+      player = Player.new("B", 2, -1)
+      result = game.player_piece("F8", player)
+      expect(result).not_to eql(nil)
+    end
 
 
+    it "Disallows player black to move white piece" do
+      game = Game.new
+      player = Player.new("B", 2, -1)
+      result = game.player_piece("A2", player)
+      expect(result).to eql(nil)
+    end
+
+    it "Disallows player white to move black piece" do
+      game = Game.new
+      player = Player.new("W", 1, 1)
+      result = game.player_piece("A7", player)
+      expect(result).to eql(nil)
+    end
+
+    it "Returns coordinate input if coordinate is empty" do
+      game = Game.new
+      player = Player.new("W", 1, 1)
+      result = game.player_piece("C5", player)
+      expect(result).to eql("C5")
+    end
 
   end
+
+  describe '#piece_at_that_pos?' do
+    it "Returns true for piece at that position" do 
+      game = Game.new
+      actual = game.piece_at_that_pos?("B2")
+      expect(actual).to eql(true)
+    end
+
+    it "Returns false for piece at that position" do 
+      game = Game.new
+      actual = game.piece_at_that_pos?("E6")
+      expect(actual).to eql(false)
+    end
+
+  end
+
+
+
 end
