1cd1fd1b664d291bfd3d84e758bbfef8c93ccd2f
Kian <Ksadeghi@sas.upenn.edu>
2020-10-31 19:27:10 -0400

    Store all captured pieces

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,16 @@
 class Player
 
-  attr_reader :color, :number, :direction
+  attr_reader :color, :number, :direction, :captured_pieces
 
   def initialize(color, number, direction)
     @color = color
     @number = number
     @direction = direction
+    @captured_pieces = []
+  end
+
+  def add_captured_piece(piece)
+    captured_pieces.push(piece)
   end
 
   def make_guess
