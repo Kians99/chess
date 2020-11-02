5968313034dbdbf44e0f02465a095df6de4100cf
Kian <Ksadeghi@sas.upenn.edu>
2020-11-02 13:36:18 -0500

    Find all possible rook moves

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,7 +1,22 @@
 require_relative 'piece'
 
 class Rook < Piece
+
+  attr_reader :name
+
   def initialize(color, unicode)
     super(color, unicode)
+    @name = 'rook'
   end
+
+  def valid_move(start_coord, end_coord, board, _player)
+    
+    to_numerical = super(start_coord, end_coord)
+    vertical_moves = pos_vertical_moves(to_numerical[0], board, 1) + pos_vertical_moves(to_numerical[0], board, -1)
+    horizontal_moves = pos_horizontal_moves(to_numerical[0], board, 1) + pos_horizontal_moves(to_numerical[0], board, -1)
+    moves = vertical_moves + horizontal_moves
+    moves.include?(to_numerical[1])
+    
+  end
+
 end
