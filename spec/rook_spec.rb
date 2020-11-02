1ff42a8b04fb6df366dfcb0bcef8c07bb44565aa
Kian <Ksadeghi@sas.upenn.edu>
2020-11-02 16:13:40 -0500

    Add tests for pos_horizontal_moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -40,6 +40,46 @@ describe Rook do
     
   end
 
+  describe  '#pos_horizontal_moves' do
+
+    it 'Returns possible moves for white rook in opening position' do
+      rook = Rook.new("W", "\u2656")
+      board = Board.new
+      pos_moves_pos = rook.pos_horizontal_moves([1, 1], board, 1)
+      pos_moves_neg = rook.pos_horizontal_moves([1, 1], board, -1)
+      total = pos_moves_pos + pos_moves_neg
+      expect(total).to contain_exactly([2,1])
+    end
+
+    it 'Returns possible moves for black rook in opening position' do
+      rook = Rook.new("B", "\u265C")
+      board = Board.new
+      pos_moves_pos = rook.pos_horizontal_moves([1, 8], board, 1)
+      pos_moves_neg = rook.pos_horizontal_moves([1, 8], board, -1)
+      total = pos_moves_pos + pos_moves_neg
+      expect(total).to contain_exactly([2,8])
+    end
+
+    it 'Returns possible moves for white rook in non-opening position' do
+      rook = Rook.new("W", "\u2656")
+      board = Board.new
+      pos_moves_pos = rook.pos_horizontal_moves([7, 5], board, 1)
+      pos_moves_neg = rook.pos_horizontal_moves([7, 5], board, -1)
+      total = pos_moves_pos + pos_moves_neg
+      expect(total).to contain_exactly([8,5], [6,5], [5,5], [4,5], [3,5], [2,5], [1,5])
+    end
+
+    it 'Returns possible moves for black rook in non-opening position' do
+      rook = Rook.new("B", "\u265C")
+      board = Board.new
+      pos_moves_pos = rook.pos_horizontal_moves([3, 4], board, 1)
+      pos_moves_neg = rook.pos_horizontal_moves([3, 4], board, -1)
+      total = pos_moves_pos + pos_moves_neg
+      expect(total).to contain_exactly([1,4], [2,4], [4,4], [5,4], [6,4], [7,4], [8,4])
+    end
+
+  end
+
 
 
 end
