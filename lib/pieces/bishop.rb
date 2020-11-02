a9599358aec8227737ae63aced428762c3c41146
Kian <Ksadeghi@sas.upenn.edu>
2020-11-02 16:47:21 -0500

    Add ability to move

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,8 +1,22 @@
 require_relative 'piece'
 
 class Bishop < Piece
+
+  attr_reader :name
+
   def initialize(color, unicode)
     super(color, unicode)
+    @name = 'bishop'
   end
+
+  def valid_move(start_coord, end_coord, board, _player)
+    
+    to_numerical = super(start_coord, end_coord)
+    moves = pos_diagonal_moves(to_numerical[0], board)
+    p moves
+    moves.include?(to_numerical[1])
+    
+  end
+
 end
 
