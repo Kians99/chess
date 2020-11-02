dd462baf763bca4c3f7f6d40f6f1c8e762fedf15
Kian <Ksadeghi@sas.upenn.edu>
2020-11-02 18:50:09 -0500

    Queen can now move

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,7 +1,23 @@
 require_relative 'piece'
 
 class Queen < Piece
+
+  attr_reader :name
+
   def initialize(color, unicode)
     super(color, unicode)
+    @name = "queen"
   end
+
+  def valid_move(start_coord, end_coord, board, _player)
+    
+    to_numerical = super(start_coord, end_coord)
+    diagnoal_moves = pos_diagonal_moves(to_numerical[0], board)
+    vertical_moves = pos_vertical_moves(to_numerical[0], board, 1) + pos_vertical_moves(to_numerical[0], board, -1)
+    horizontal_moves = pos_horizontal_moves(to_numerical[0], board, 1) + pos_horizontal_moves(to_numerical[0], board, -1)
+    moves = vertical_moves + horizontal_moves + diagnoal_moves
+    moves.include?(to_numerical[1])
+    
+  end
+
 end
