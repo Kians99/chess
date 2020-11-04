d05162afee666b0eed045b9e47f66401fb7b9467
Kian <Ksadeghi@sas.upenn.edu>
2020-11-04 14:53:52 -0500

    Add pawn diagonal capture

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,29 +3,34 @@ require_relative 'piece'
 class Pawn < Piece
 
   attr_reader :name
+  attr_accessor :first_move
 
   def initialize(color, unicode)
     super(color, unicode)
     @name = 'pawn'
+    @first_move = true
   end
 
   #ADD ABILIRY TO MOVE DIAGNOLALY 
 
-  def valid_move(start_coord, end_coord, player)
+  def valid_move(start_coord, end_coord, board, player)
     #to_numerical[0] = start coordinate
     #to_numerical[1] = end coordinate
     to_numerical = super(start_coord, end_coord)
-    moves = possible_moves(to_numerical[0], player)
+    moves = possible_moves(to_numerical[0], player, board)
     moves.include?(to_numerical[1])
   end
 
-  def possible_moves(coord, player, pos_moves = [])
+  def possible_moves(coord, player, board, pos_moves = [])
     first_pos_move = coordinates(player.direction * 1, coord)
     pos_moves.push(first_pos_move)
-    if coord[1] == 2 || coord[1] == 7
+    if self.first_move
       second_pos_move = coordinates(player.direction * 2, coord)
       pos_moves.push(second_pos_move)
+      self.first_move = false
     end
+    pos_moves += diagonal(player.direction, coord, board)
+
     pos_moves
   end
 
@@ -35,6 +40,30 @@ class Pawn < Piece
     new_coord[0] = orig_coord[0]
     new_coord[1] = orig_coord[1] + direction
     new_coord
+  end
+
+  def diagonal(direction, orig_coord, board, new_coords = [])
+
+    positive_y = orig_coord[1] + (direction * 1)
+    positive_x = orig_coord[0] + 1
+    negative_x = orig_coord[0] - 1
+
+    if valid_coord?(positive_x, positive_y)
+
+      pos_cord = [positive_x, positive_y]
+      new_coords.push(pos_cord) if board.chess_board[Piece.translate_to_algebraic(pos_cord)] != ' '
+    
+    end
+
+    if valid_coord?(negative_x, positive_y)
+      other_pos_cord = [negative_x, positive_y]  
+      new_coords.push(other_pos_cord) if board.chess_board[Piece.translate_to_algebraic(other_pos_cord)] != ' '
+    end
+
+
+    new_coords
+    
+  
   end
 
 end
