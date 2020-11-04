4945d8d218eebdb48c906c0a34c517bab78bb110
Kian <Ksadeghi@sas.upenn.edu>
2020-11-04 14:55:58 -0500

    Ensure pawn diagonal functionality

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -5,29 +5,78 @@ describe Pawn do
     it 'Returns possible moves for white pawn in opening position' do
       pawn = Pawn.new("W", "\u2659")
       player = Player.new("W", 1, 1)
-      pos_moves = pawn.possible_moves([4,2], player)
+      board = Board.new
+      pos_moves = pawn.possible_moves([4,2], player, board)
       expect(pos_moves).to eql([[4,3],[4,4]])
     end
 
     it 'Returns possible moves for white pawn in non-opening position' do
       pawn = Pawn.new("W", "\u2659")
+      pawn.first_move = false
       player = Player.new("W", 1, 1)
-      pos_moves = pawn.possible_moves([4,4], player)
+      board = Board.new
+      pos_moves = pawn.possible_moves([4,4], player, board)
       expect(pos_moves).to eql([[4,5]])
     end
 
     it 'Returns possible moves for black pawn in opening position' do
       pawn = Pawn.new("B", "\u2659")
       player = Player.new("B", 2, -1)
-      pos_moves = pawn.possible_moves([3,7], player)
+      board = Board.new
+      pos_moves = pawn.possible_moves([3,7], player, board)
       expect(pos_moves).to eql([[3,6],[3,5]])
     end
 
     it 'Returns possible moves for black pawn in non-opening position' do
       pawn = Pawn.new("B", "\u2659")
+      pawn.first_move = false
       player = Player.new("B", 2, -1)
-      pos_moves = pawn.possible_moves([3,6], player)
+      board = Board.new
+      pos_moves = pawn.possible_moves([3,6], player, board)
       expect(pos_moves).to eql([[3,5]])
+    end
+
+    it 'Returns possible moves for capturing another piece player 1' do
+      pawn = Pawn.new("W", "\u2659")
+      pawn.first_move = false
+      player = Player.new("W", 1, 1)
+      board = Board.new
+      pos_moves = pawn.possible_moves([4,1], player, board)
+      expect(pos_moves).to eql([[4,2], [5,2], [3,2]])
+    end
+
+
+    it 'Returns possible moves for capturing another piece player 2' do
+      pawn = Pawn.new("B", "\u2659")
+      pawn.first_move = false
+      player = Player.new("B", 2, -1)
+      board = Board.new
+      pos_moves = pawn.possible_moves([6,8], player, board)
+      expect(pos_moves).to eql([[6,7], [7,7], [5,7]])
+    end
+
+    it 'Returns possible moves for capturing another piece player 1 one side' do
+      pawn = Pawn.new("W", "\u2659")
+      pawn.first_move = false
+      player = Player.new("W", 1, 1)
+      board = Board.new
+      piece = board.chess_board['C2']
+      board.chess_board['C2'] = ' '
+      board.chess_board['C3'] = piece
+      pos_moves = pawn.possible_moves([4,2], player, board)
+      expect(pos_moves).to eql([[4,3], [3,3]])
+    end
+
+    it 'Returns possible moves for capturing another piece player 2 one side' do
+      pawn = Pawn.new("B", "\u2659")
+      pawn.first_move = false
+      player = Player.new("B", 2, -1)
+      board = Board.new
+      piece = board.chess_board['D7']
+      board.chess_board['D7'] = ' '
+      board.chess_board['D6'] = piece
+      pos_moves = pawn.possible_moves([5,7], player, board)
+      expect(pos_moves).to eql([[5,6], [4,6]])
     end
 
 
