8297fffa9548435d069b09c8006eda685a9fdaa1
Kian <Ksadeghi@sas.upenn.edu>
2020-11-06 19:54:14 -0500

    Add queue for all moves

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,14 @@
 class Player
 
   attr_reader :color, :number, :direction, :captured_pieces
+  attr_accessor :queue_all_moves
 
   def initialize(color, number, direction)
     @color = color
     @number = number
     @direction = direction
     @captured_pieces = []
+    @queue_all_moves = []
   end
 
   def add_captured_piece(piece)
